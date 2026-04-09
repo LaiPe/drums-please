a533a1648156cc670df4ace51785c34be71ab1f9
Léo Peyronnet <peyronnet.leo@gmail.com>
2026-04-09 21:18:25 +0200

    feat : init. gatsby redesign

---
+++ src/layouts/footer.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <p>Copyright 2024 Drums Please</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
+import Layout from "../layouts/layout"
+
+const IndexPage: React.FC<PageProps> = () => {
+  return (
+    <Layout>
+      <h2>Welcome to Drums Please!</h2>
+      <p>Your one-stop shop for all things drums. Explore our wide selection of drum kits, accessories, and more. Whether you're a beginner or a seasoned drummer, we have something for everyone. Start your drumming journey with us today!</p>
+    </Layout>
+  )
+}
+
+export default IndexPage
+
+export const Head: HeadFC = () => <title>Home Page</title>
