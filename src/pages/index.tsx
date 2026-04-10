b44373b3706f1e4f44273ac53ca64dde0fff2088
Léo Peyronnet <peyronnet.leo@gmail.com>
2026-04-10 15:33:01 +0200

    feat: update ProductCard and index styles for responsive design; add about section with content and image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout>
 
-      <section className={styles.hero}>
+      <section className={styles.heroSection}>
         <BackgroundMedia mediaType="video" mediaSrc="/videos/video-background-index.mp4" childrenClassName={styles.heroGradient}>
           <h2 className={styles.heroTitle}>
             <span>{heroTitle[0]}</span>
@@ -44,7 +44,7 @@ const IndexPage: React.FC<PageProps> = () => {
         </BackgroundMedia>
       </section>
 
-      <section className={styles.productsGrid}>
+      <section className={styles.productsSection}>
         {products.map((product) => (
           <ProductCard
             key={product.title}
@@ -55,7 +55,43 @@ const IndexPage: React.FC<PageProps> = () => {
         ))}
       </section>
 
-
+      <section className={styles.aboutSection}>
+        <img
+          className={styles.aboutImage}
+          src="/img/fond-presentation.png"
+        />
+        <div className={styles.aboutContent}>
+          <p>
+            Tout a commencé il y a quelques années, dans un coin de mon
+            appartement, où les murs résonnaient des rythmes que je frappais
+            avec ma batterie. La musique a toujours été
+            <em>ma muse</em>, <em>mon refuge</em>, mais l'espace limité ne
+            permettait pas d'exprimer pleinement ma créativité.
+          </p>
+          <p>
+            Face à cette contrainte, j'ai cherché une solution. Et c'est ainsi
+            qu'est née l'idée de
+            <strong>Drums Please</strong>. Je me suis dit : pourquoi ne pas
+            <strong>rendre les percussions accessibles à tous</strong>, peu
+            importe l'espace dont ils disposent ? Pourquoi ne pas permettre à
+            chacun de vivre la magie des rythmes, sans avoir à investir dans un
+            instrument encombrant ?
+          </p>
+          <p>
+            Ce qui nous différencie chez Drums Please, c'est notre
+            <strong>engagement envers nos clients</strong>. Nous ne sommes pas
+            simplement une entreprise de location d'instruments. Nous sommes une
+            <strong>communauté de passionnés</strong>, prêts à accompagner
+            chaque musicien, débutant ou expérimenté, dans son voyage musical.
+          </p>
+          <p>
+            Aujourd'hui, nous avons grandi, mais notre esprit demeure le même.
+            Nous restons fidèles à nos valeurs fondatrices :
+            <strong>l'accessibilité</strong>, <strong>la qualité</strong> et
+            <strong>le service personnalisé</strong>.
+          </p>
+        </div>
+      </section>
     </Layout>
   )
 }
