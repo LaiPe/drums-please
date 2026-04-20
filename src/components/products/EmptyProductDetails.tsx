4ce13b8cef194ded7b8b127f8ee64a16ff20863e
Léo Peyronnet <peyronnet.leo@gmail.com>
2026-04-20 19:29:07 +0200

    fix: enhance EmptyProductDetails styles with line-height adjustments

--- src/components/products/EmptyProductDetails.tsx
+++ src/components/products/EmptyProductDetails.tsx
@@ -5,7 +5,7 @@ import Button from "@/components/inputs/Button"
 export default function EmptyProductDetails() {
     return (
         <div className={styles.container}>
-            <h3 className={styles.title}>It's empty here!</h3>
+            <h3 className={`${styles.title} ${styles.lineHeight}`}>It's empty here!</h3>
             <p className={styles.message}>Hey administrator ! Please add some products to this category.</p>
             <Button type="link" href="/admin/products">Go to Admin Panel</Button>
         </div>
